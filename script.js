9a3d6f9f25eabfd3c7523969738615d57b0ac66e
YimitriY <christianvincelle@gmail.com>
2025-02-14 18:04:01 +0000

    Valo web

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".boton").forEach(boton => {
+    boton.addEventListener("click", function() {
+        let video = document.querySelector("#miVideo");
+        video.style.width = 600%
+});
+});
